78584d45f92ed20af4a99b9539db9772f159a50d
Marco Custodio <marco.antonio.custodio@gmail.com>
2021-09-03 02:04:36 +0100

    Update app.js
    Print the knock-knock joke and add the punchline to a variable

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 // Knock knock jokes 
 const knockKnockObject = {
-    knockMessage : "Knock-knock ...",
-    whosthereMessage : "Who's there?",
-    punchLine : []
+    knockMessage : "Knock-knock. Who's There?",
+    punchLine : [
+                    ["Says","Says me, that's who!"],
+                    ["Gouda","Gouda knock knock jokes, don't you think?"],
+                    ["Honeydew","Honeydew you wanna dance?"],
+                    ["Europe","No, YOU'RE a poo!"],
+                    ["Omelette","Omelette you finish."],
+                    ["Weekend","Weekend do anything we want!"],
+                    ["Taco","Taco to you later. It's taking too long for you to open the door."],
+                    ["Hawaii","I'm fine, Hawaii you?"],
+                    ["Voodoo","Voodoo you think you are, asking me so many questions?"],
+                    ["Mustache","Mustache you a question, but I'll shave it for later"]
+                ]   
 }
+
+const randomNum = (num) => Math.floor(Math.random() * num)
+
+const punchLine = knockKnockObject.punchLine[randomNum(knockKnockObject.punchLine.length)]
+
+console.log(`${knockKnockObject.knockMessage} \n${punchLine[0]}.\n${punchLine[0]} who?\n${punchLine[1]}`)
