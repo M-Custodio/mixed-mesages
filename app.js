a08cc658217ae06266c586b7f177521dd034ba39
Marco Custodio <marco.antonio.custodio@gmail.com>
2021-09-03 01:47:00 +0100

    Update app.js
    Random Function and object structure change

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 // Knock knock jokes 
 const knockKnockObject = {
-    knockMessage : "Knock-knock ...",
-    whosthereMessage : "Who's there?",
-    punchLine : []
+    knockMessage : "Knock-knock. Who's There?",
+    punchLine : [
+                    ["Says","Says me, that's who!"],
+                    ["Gouda","Gouda knock knock jokes, don't you think?"],
+                    ["Honeydew","Honeydew you wanna dance?"],
+                    ["Europe","No, YOU'RE a poo!"],
+                    ["Omelette","Omelette you finish."],
+                    ["Weekend","Weekend do anything we want!"]
+                ]   
 }
+
+const randomNum = (num) => Math.floor(Math.random() * num)
+
+
